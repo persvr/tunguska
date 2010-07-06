2d7bb36c46e7a2f9163b2c192ea377552c177d9a
Kris Zyp <kriszyp@gmail.com>
2010-07-06 07:26:56 -0600

    Use promise-io if available

--- tests/hub.js
+++ tests/hub.js
@@ -56,7 +56,7 @@ exports.listenForSubscribe= function(finished){
 	hub.unsubscribe("foo", listener);
 };
 
-print = require("commonjs-utils/sys").print;
+print = require("promised-io/process").print;
 if (require.main === module){
 	for(var i in exports){
 		if(i.substring(0,4) == "test"){
